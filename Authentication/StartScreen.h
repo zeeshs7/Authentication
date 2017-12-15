69de973a64c1bfbbbf6da3bf0b7cf7b6140809a3
Zeshaan Saeed <zeshaansaeed@zeshaans-macbook-pro.local>
2017-12-15 09:25:32 +0000

    added switch function

--- Authentication/StartScreen.h
+++ Authentication/StartScreen.h
@@ -8,15 +8,38 @@
 
 #import <UIKit/UIKit.h>
 
-NSInteger addition;
-NSInteger subtraction;
-NSInteger multiplication;
-NSInteger division;
+NSInteger topic1;
+NSInteger topic2;
+NSInteger topic3;
+NSInteger topic4;
 
 
 @interface StartScreen : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *addition1;
+
+@property (weak, nonatomic) IBOutlet UIButton *subtraction2;
+
+@property (weak, nonatomic) IBOutlet UIButton *multiplication3;
+
+@property (weak, nonatomic) IBOutlet UIButton *division4;
+
 @property (weak, nonatomic) IBOutlet UILabel *mathematicQuizLabel;
+
+
+- (IBAction)additionButton:(id)sender;
+
+- (IBAction)subtractionButton:(id)sender;
+
+- (IBAction)multiplicationButton:(id)sender;
+
+- (IBAction)divisionButton:(id)sender;
+
+
+
+
+
+
 
 
 
--- Authentication/StartScreen.m
+++ Authentication/StartScreen.m
@@ -16,6 +16,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+        topic1 = arc4random() %1;
+        topic2 = arc4random() %1;
+        topic3 = arc4random() %1;
+        topic4 = arc4random() %1;
+    
+    //The switch function is used to clarify the name of the four different categories used as show below
+    
+    switch (topic1) {
+            
+        case 0:
+            [_addition1 setTitle:@"Addition" forState:UIControlStateNormal];
+            topic1 = 0;
+            break;
+            
+        default:
+            break;
+    }
+    
+    switch (topic2) {
+            
+        case 0:
+            [_subtraction2 setTitle:@"Subtraction" forState:UIControlStateNormal];
+            topic2 = 0;
+            break;
+
+        default:
+            break;
+    }
+    
+    switch (topic3) {
+            
+        case 0:
+            [_multiplication3 setTitle:@"Multiplication" forState:UIControlStateNormal];
+            topic3 = 1;
+            break;
+            
+        default:
+            break;
+    }
+    
+    switch (topic4) {
+            
+        case 0:
+            [_division4 setTitle:@"Division" forState:UIControlStateNormal];
+            topic4 = 1;
+            break;
+            
+        default:
+            break;
+    }
+    
+    
+    
     // Do any additional setup after loading the view.
 }
 
@@ -34,6 +88,32 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+- (IBAction)additionButton:(id)sender {
+    
+    [[NSUserDefaults  standardUserDefaults] setInteger:topic1 forKey:@"TopicSaved"];
+    
+}
+
+- (IBAction)subtractionButton:(id)sender {
+    
+    [[NSUserDefaults  standardUserDefaults] setInteger:topic2 forKey:@"TopicSaved"];
+
+}
+
+- (IBAction)multiplicationButton:(id)sender {
+    
+    [[NSUserDefaults  standardUserDefaults] setInteger:topic3 forKey:@"TopicSaved"];
+
+}
+
+- (IBAction)divisionButton:(id)sender {
+    
+    [[NSUserDefaults  standardUserDefaults] setInteger:topic4 forKey:@"TopicSaved"];
+
+}
+
 
 
 
