7da34dc11d03c5fe95e1b977d6f1bf87892d8f73
Zeshaan Saeed <zeshaansaeed@eduroam-108-66.dyn.brad.ac.uk>
2017-12-15 10:07:04 +0000

    Final commit before inputting questions

--- Authentication/QuestionScreen.h
+++ Authentication/QuestionScreen.h
@@ -8,16 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
+BOOL correctAnswer1;
+BOOL correctAnswer2;
+BOOL correctAnswer3;
+BOOL correctAnswer4;
+
+NSInteger selectedTopic;
+
+int selectedQuestion;
+
+
+
 @interface QuestionScreen : UIViewController
+
+
 
 @property (weak, nonatomic) IBOutlet UILabel *mathsQuestionLabel;
 
 @property (strong, nonatomic) IBOutlet UIView *answer1;
+
 @property (weak, nonatomic) IBOutlet UIButton *answer2;
+
 @property (weak, nonatomic) IBOutlet UIButton *answer3;
+
 @property (weak, nonatomic) IBOutlet UIButton *answer4;
 
 @property (weak, nonatomic) IBOutlet UILabel *Correct;
+
 
 - (IBAction)button1:(id)sender;
 
@@ -26,6 +43,11 @@
 - (IBAction)button3:(id)sender;
 
 - (IBAction)button4:(id)sender;
+
+
+
+
+
 
 
 
